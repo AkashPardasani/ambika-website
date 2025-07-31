bdec7d78442ff963cd9ff175f2cae378a70ae672
ParthVijay13 <parthvijay2004@gmail.com>
2025-07-31 15:41:21 +0530

    cross origin issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -30,7 +30,7 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://www.houseofambika.com, https://houseofambika.com"
+            value: "*"
           },
           {
             key: "Access-Control-Allow-Methods",
